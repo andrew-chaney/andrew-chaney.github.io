37bef1f81c1c45dcc9e949c1a2e5e752942c5060
Andrew Chaney <andrewchaney24@gmail.com>
2022-10-16 12:42:06 -0600

    Custom validation for contact page to work on mobile and desktop.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,26 @@
+<script>
+    import { emailValidator, requiredValidator } from './validators.js';
+    import { createFieldValidator } from './validation.js';
+
+    // Validation for email-field
+    const [ email_validity, email_validate ] = createFieldValidator(
+        requiredValidator(), emailValidator()
+    );
+    let email = null;
+
+    // Validation for subject-field
+    const [ subject_validity, subject_validate ] = createFieldValidator(
+        requiredValidator()
+    );
+    let subject = null;
+
+    // Validation for message-field
+    const [ message_validity, message_validate ] = createFieldValidator(
+        requiredValidator()
+    );
+    let message = null;
+</script>
+
 <title>Contact Me</title>
 
 <section>
@@ -7,17 +30,66 @@
         <form action="https://getform.io/f/07a31429-d42c-4efe-a66f-3c9f69a9e2ff" method="POST" class="space-y-8">
             <div>
                 <label for="email" class="block mb-2 text-sm font-medium">Your email</label>
-                <input type="email" name="email" placeholder="someone@example.com" class="input input-bordered text-sm w-full p-2.5 bg-base-200" required/>
+                <input
+                    type="email"
+                    bind:value={email}
+                    class:input-error={!$email_validity.valid && $email_validity.dirty}
+                    class:input-success={$email_validity.valid}
+                    name="email"
+                    placeholder="someone@example.com"
+                    class="input input-bordered text-sm w-full p-2.5 bg-base-200"
+                    use:email_validate={email}
+                />
+                {#if $email_validity.dirty && !$email_validity.valid}
+                <span class="label-text-alt text-error">
+                    Please enter a valid email.
+                </span>
+                {/if}
             </div>
             <div>
                 <label for="subject" class="block mb-2 text-sm font-medium">Subject</label>
-                <input type="text" name="subject" placeholder="Something catchy here." class="input input-bordered text-sm w-full p-2.5 bg-base-200" required/>
+                <input
+                    id="subject-field"
+                    type="text"
+                    bind:value={subject}
+                    class:input-error={!$subject_validity.valid && $subject_validity.dirty}
+                    class:input-success={$subject_validity.valid}
+                    name="subject"
+                    placeholder="Something catchy here."
+                    class="input input-bordered text-sm w-full p-2.5 bg-base-200"
+                    use:subject_validate={subject}
+                />
+                {#if $subject_validity.dirty && !$subject_validity.valid}
+                <span class="label-text-alt text-error">
+                    Please enter a subject.
+                </span>
+                {/if}
             </div>
             <div class="sm:col-span-2">
                 <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Your message</label>
-                <textarea name="message" class="textarea textarea-bordered text-sm w-full p-2.5 bg-base-200" placeholder="Leave a comment..."></textarea>
+                <textarea
+                    type="text"
+                    bind:value={message}
+                    class:textarea-error={!$message_validity.valid && $message_validity.dirty}
+                    class:textarea-success={$message_validity.valid}
+                    name="message"
+                    placeholder="Leave a comment..."
+                    class="textarea textarea-bordered text-sm w-full p-2.5 bg-base-200"
+                    use:message_validate={message}
+                />
+                {#if $message_validity.dirty && !$message_validity.valid}
+                <span class="label-text-alt text-error">
+                    Please enter a subject.
+                </span>
+                {/if}
             </div>
-            <button type="submit" class="btn btn-outline">Send</button>
+            <button
+                type="submit"
+                class="btn btn-outline"
+                disabled={!$email_validity.valid || !$subject_validity.valid || !$message_validity.valid}
+                >
+                Send
+            </button>
         </form>
     </div>
 </section>
